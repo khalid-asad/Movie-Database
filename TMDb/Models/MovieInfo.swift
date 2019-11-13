28d4fd0d859a947d0f2d180f6786f7b6be51e228
Khalid Asad <khalidasad93@gmail.com>
2019-11-12 20:47:43 -0800

    Created Codable objects

--- TMDb/Models/MovieInfo.swift
+++ TMDb/Models/MovieInfo.swift
@@ -21,6 +21,54 @@ struct MovieInfo: Codable {
     }
 }
 
+struct MovieSearchQuery: Codable {
+    var page: Int
+    var totalResults: Int
+    var totalPages: Int
+    var results: [MovieSearchResult]
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case results
+    }
+}
+
+struct MovieSearchResult: Codable {
+    var popularity: Double
+    var voteCount: Int
+    var video: Bool
+    var posterPath: String?
+    var id: Int
+    var adult: Bool
+    var backdropPath: String?
+    var originalLanguage: String
+    var originalTitle: String
+    var genreIDs: [Int]
+    var title: String
+    var voteAverage: Double
+    var overview: String
+    var releaseDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case popularity
+        case voteCount = "vote_count"
+        case video
+        case posterPath = "poster_path"
+        case id
+        case adult
+        case backdropPath = "backdrop_path"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case genreIDs = "genre_ids"
+        case title
+        case voteAverage = "vote_average"
+        case overview
+        case releaseDate = "release_date"
+    }
+}
+
 enum PopularMovies: String, CaseIterable {
     case all = "All"
     case goodBoys = "Good Boys"
